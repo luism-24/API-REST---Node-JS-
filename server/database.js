f5754714d6160bc0fa1034ef527f585f2ca92ed9
luism-24 <lumimarriaga@gmail.com>
2018-10-09 13:33:11 -0500

    mlab add to the database config

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); 
-const URI = 'mongodb://localhost/dojo-example'; 
+const URI = 'mongodb://example:l9910240792*@ds127293.mlab.com:27293/api-rest-nodejs-database'; 
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
