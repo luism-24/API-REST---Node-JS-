04ad0858ca740d1c6bcab3f8c05a707527f73a2f
luism-24 <lumimarriaga@gmail.com>
2018-10-10 16:34:44 -0500

    re undo changes

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -7,7 +7,13 @@ employeeCtrl.getEmployees = async (req, res) => {
 }
 
 employeeCtrl.createEmployee = async (req, res) => {
-    const employee = new Employee(req.body)
+    const employee = new Employee({
+        name: req.body.name, 
+        position: req.body.position,
+        office: req.body.office, 
+        salary: req.bodt.salary
+    })
+    
     await employee.save()
     res.json({
         'status': 'Employee Saved'
